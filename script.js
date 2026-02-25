c39081ec79d17c2cc60b97298219dace2174a143
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-02-25 13:31:11 +0530

    Different names for variable names and parameters to avoid conflicts

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getComputerChoice() {
-  let computerChoice =  Math.random()*100;
-  if (computerChoice <= 33.33) {
+  let computerTurn =  Math.random()*100;
+  if (computerTurn <= 33.33) {
   return "rock";
-} else if (computerChoice <= 66.66) {
+} else if (computerTurn <= 66.66) {
   return "paper";
-} else if (computerChoice <= 99.99) {
+} else if (computerTurn <= 99.99) {
   return "scissor";
 } 
 }
@@ -12,5 +12,15 @@ function getComputerChoice() {
 getComputerChoice();
 
 function getHumanChoice() {
-  
+  let humanChoice = prompt("Choose between rock, paper and scissor");
+  return humanChoice;
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+  let humanPlay = humanChoice.toLowerCase();
+  console.log("You lose! Paper beats rock");
 }
