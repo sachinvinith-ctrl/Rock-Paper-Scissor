6b1553b9e2e18f142fc65f331e71f313d00b2fe8
Sachin Vinith <233778418+sachinvinith-ctrl@users.noreply.github.com>
2026-02-27 12:04:55 +0530

    Call game 5 rounds and display alert to show player what computer chose

--- script.js
+++ script.js
@@ -30,24 +30,30 @@ function playGame() {
   console.log(computerChoice)
   if (computerChoice === "paper" && humanPlay === "rock") {
       console.log("You lose! Paper beats rock");
+      alert("Computer chose paper");
       ++computerScore;
     } else if (computerChoice === "rock" && humanPlay === "paper") {
       console.log("You Win! Paper beats rock");
+      alert("Computer chose rock");
       ++humanScore;
  }    else if (computerChoice === "paper" && humanPlay === "scissor") {
       console.log("You Win! Scissor beats paper");
+      alert("computer chose paper");
       ++humanScore;
 }     else if (computerChoice === "scissor" && humanPlay === "paper") {
       console.log("You Lose! Scissor beats paper");
+      alert("Computer chose scissor");
       ++computerScore;
 }    else if (computerChoice === "rock" && humanPlay === "scissor") {
-      console.log("You Lose! Rock beats Scissor"); 
+      console.log("You Lose! Rock beats Scissor");
+      alert("Computer chose rock");
       ++computerScore;
 }   else if (computerChoice === "scissor" && humanPlay === "rock") {
       console.log("You Win! Rock beats scissor");
+      alert("Computer chose scissor");
       ++humanScore;
 }   else { console.log("This round is tied");
-    
+           alert("Computer chose the same as you");
 }
   
 } 
@@ -56,6 +62,8 @@ playRound(humanChoice(), computerChoice());
 console.log(humanScore, computerScore);
 }
 
+playGame();
+playGame();
 playGame();
 playGame();
 playGame();
